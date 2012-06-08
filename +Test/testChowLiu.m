fed95ad0a89e24d341b13f75b8f3d6dad95b7d95
koenigst <koenigst@student.ethz.ch>
2012-06-08 17:14:01 +0200

    Stable: added complex graph.

--- +Test/testChowLiu.m
+++ +Test/testChowLiu.m
@@ -21,3 +21,18 @@ for c=1:length(classes)
     node.connect(parents.(classes{c}),conn);
     g.addNode(node);
 end
+
+
+gplus=NetFunc.BNTGraph();
+states.occ={'0';'1';'2+'};
+states.loc={'here';'there'};
+states.corr={'false';'positive'};
+extConn.occ={'occ'};
+extConn.loc={'occ';'loc'};
+intConn.loc={'occ'};
+intConn.corr={'occ';'loc'};
+for c=1:length(classes)
+    node=NetFunc.BNTStructureNode(classes{c},intConn,states);
+    node.connect(parents.(classes{c}),extConn);
+    gplus.addNode(node);
+end

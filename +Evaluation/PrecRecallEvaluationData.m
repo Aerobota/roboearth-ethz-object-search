84c42e3ea38a68ef9cd61ef080b0a3ba0950310d
denahiro <denahiro@hotmail.com>
2012-11-13 12:05:05 +0100

    Stable: completed comments on Evaluation.

--- +Evaluation/PrecRecallEvaluationData.m
+++ +Evaluation/PrecRecallEvaluationData.m
@@ -1,19 +1,25 @@
 classdef PrecRecallEvaluationData<Evaluation.EvaluationData
-    %PRECRECALLEVALUATIONDATA Summary of this class goes here
-    %   Detailed explanation goes here
+    %PRECRECALLEVALUATIONDATA Generates precision recall curves
+    %   This is a handler class that generates precision recall plots. This
+    %   class extends Evaluation.EvaluationData and has no public methods
+    %   of its own.
+    %
+    %See also EVALUATION.EVALUTIONDATA
     
     methods(Access='protected')
         function newCurve=addDataImpl(~,newData,classSubset)
             if nargin<3
                 classSubset=true(1,size(newData.tp,2));
             end
-%             index=length(obj.curves)+1;
+            
+            % save the selected statistics
             newCurve.tp=newData.tp(:,classSubset);
             newCurve.fp=newData.fp(:,classSubset);
             newCurve.pos=newData.pos(:,classSubset);
         end
         
         function drawImpl(obj)
+            % For each curve compute precision and recall
             for c=length(obj.curves):-1:1
                 tmpTp=sum(obj.curves(c).tp,2);
                 tmpFp=sum(obj.curves(c).fp,2);
@@ -21,6 +27,7 @@ classdef PrecRecallEvaluationData<Evaluation.EvaluationData
                 precision(:,c)=tmpTp./(tmpTp+tmpFp+eps);
             end
             
+            % plot
             hold(obj.myAxes,'on')
             for c=1:length(obj.curves)
                 plot(obj.myAxes,tpRate(:,c),precision(:,c),'Color',obj.curves(c).colour,...
@@ -28,7 +35,9 @@ classdef PrecRecallEvaluationData<Evaluation.EvaluationData
             end
             hold(obj.myAxes,'off')
 
+            % Limit the axis as this is a well defined area
             axis(obj.myAxes,[0 1 0 1])
+            % Add text
             legend(obj.myAxes,{obj.curves.name},'location','northeast')
             xlabel(obj.myAxes,'recall')
             ylabel(obj.myAxes,'precision')
--- +Evaluation/ThresholdOccurrenceEvaluator.m
+++ +Evaluation/ThresholdOccurrenceEvaluator.m
@@ -1,12 +1,17 @@
 classdef ThresholdOccurrenceEvaluator<Evaluation.OccurrenceEvaluator
-    %THRESHOLDOCCURRENCEEVALUATOR Summary of this class goes here
-    %   Detailed explanation goes here
+    %THRESHOLDOCCURRENCEEVALUATOR Decides occurrence by thresholding
+    %   This class implements the decisionImpl method by comparing the
+    %   conditional probability with a set of thresholds.
     
     methods
         function decisions=decisionImpl(obj,myDependencies)
             tmpSize=size(myDependencies.condProb);
+            % Make a decision for every threshold and every possibility of
+            % observed random variables.
             decisions=ones([length(obj.thresholds),tmpSize(2:end)]);
+            % Input the conditional probabilities
             tmpCP=repmat(myDependencies.condProb(2,:),[length(obj.thresholds) ones(1,ndims(decisions)-1)]);
+            % Make the decisions by thresholding
             decisions(tmpCP>=repmat(obj.thresholds,[1 size(tmpCP,2)]))=2;
         end
     end

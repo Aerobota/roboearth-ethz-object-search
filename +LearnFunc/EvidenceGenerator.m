c2f723c7d47d7e7985fa00e4b034cc58d7d05188
koenigst <koenigst@student.ethz.ch>
2012-11-15 10:12:11 +0100

    Stable: started commenting on evidence generators.

--- +LearnFunc/EvidenceGenerator.m
+++ +LearnFunc/EvidenceGenerator.m
@@ -1,4 +1,8 @@
 classdef EvidenceGenerator<handle
+    %EVIDENCEGENERATOR Abstract base class
+    %   This base class exists only for structure. It defines the method
+    %   getEvidence but doesn't place any restrictions on input or output
+    %   arguments other than the first and second input arguments.
     methods(Abstract)
         evidence=getEvidence(obj,data,varargin);
     end
